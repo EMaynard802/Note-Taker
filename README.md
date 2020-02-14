22e2b9d07518acb41d1d6f511259c6b081bbfba2
Eleanor Maynard <el_mynrd@sbcglobal.net>
2020-02-14 18:07:47 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Note-Taker
-This is an application that can be used to write, save, and delete notes. 
+
+## Description
+This is an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
+
+## Installation
+In your terminal type:
+```git clone https://github.com/EMaynard802/Note-Taker.git```
+* Open in Visual Studio to build and run.
+* For further instructions, visit https://help.github.com.
+
+## Technologies-Libraries
+Express - HTML - CSS - Bootstrap - JavaScript
+
+## Demo
+![alt text](public/assets/notetaker.gif "Demo")
+
---
+++ server.js
@@ -0,0 +1,84 @@
+// Dependencies 
+// ====================================
+const express = require("express");
+const fs = require("fs");
+const util = require("util");
+const path = require("path")
+const uuid = require("uuid/v4");
+// ====================================
+
+// Promise Modules
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+
+// Express Config 
+// ====================================
+//Tells node that we are creating an "express" server
+const app = express();
+// Set the server PORT
+const PORT = process.env.PORT || 8000;
+// Sets up the Express app to handle data parsing (Middleware)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+// ====================================
+
+// Routes
+// ====================================
+// /
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public.index.html"));
+});
+
+// notes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//api/notes
+// get
+app.get("/api/notes", async (req, res) => {
+    const db = JSON.parse(await readFileAsync(__dirname + '/db/db.json', 'utf8'));
+    return res.json(db);
+});
+// create new note post
+app.post("/api/notes", async (req, res) => {
+    try {
+        const db = JSON.parse(await readFileAsync(__dirname + '/db/db.json', 'utf8'));
+        const note = { ...req.body, id: uuid() };
+        db.push(note);
+        await writeFileAsync(__dirname + '/db/db.json', JSON.stringify(db));
+        console.log(db);
+        return res.json(db);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+// delete
+app.delete('/api/notes/:id', async (req, res) => {
+    try { const db = JSON.parse(await readFileAsync(__dirname + '/db/db.json', 'utf8'));
+    const id = req.params.id;
+    const newdb = db.filter(note => {
+        return note.id !== id;
+    });
+    await writeFileAsync(__dirname + '/db/db.json', JSON.stringify(newdb));
+        console.log(newdb);
+        return res.json(newdb)}
+    catch (err) {
+        console.log(err);
+    }
+});
+//all
+app.get("*", (req, res) => {
+    res.redirect("/");
+});
+// ====================================
+
+
+// Listener
+// ====================================
+app.listen(PORT, () =>
+    console.log(`App listening on PORT: ${PORT}`)
+)
+// ====================================
